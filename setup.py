e15508e0c8b5e2aaa8f83466be5da89b4144a35e
Feiteng <lifeiteng0422@163.com>
2024-03-30 21:34:06 +0800

    Init From https://github.com/open-mmlab/Amphion

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ns3_codec",
+    version="0.2.0",
+    packages=find_packages(where="codec"),
+    package_dir={"": "codec"},
+    package_data={"": []},
+    description="ns3_codec",
+    author="The ns3_codec Development Team",
+    long_description=open("cREADME.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    python_requires=">=3.8",
+    install_requires=[
+    ],
+    include_package_data=True,
+)
